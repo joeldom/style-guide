e08da6ab54ea3db3a0208a1806fc6462448f333e
joeld <dombekj890@gmail.com>
2017-12-18 17:07:22 -0500

    old ish

--- js/style-guide.js
+++ js/style-guide.js
@@ -100,3 +100,7 @@ $(document).ready(function () {
 //         x.style.display = "none";
 //     }
 // }
+
+$("#add").on("click", function() {
+  $("<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>").insertBefore(".push");
+});
